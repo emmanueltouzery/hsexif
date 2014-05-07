e8d0ec945be69e49a7d476700556b6e144d4b76e
Emmanuel Touzery <etouzery@gmail.com>
2014-05-07 20:48:51 +0200

    add some api docs

--- Graphics/HsExif.hs
+++ Graphics/HsExif.hs
@@ -124,7 +124,7 @@ import Numeric (showHex)
 -- If you want a string describing the contents
 -- of the value, simply use 'show'.
 data ExifValue = ExifNumber !Int
-	-- ^ An exif number. Could be short, int, signed or not.
+	-- ^ An exif number. Originally it could have been short, int, signed or not.
 	| ExifText !String
 	-- ^ ASCII text.
 	| ExifRational !Int !Int
@@ -133,11 +133,21 @@ data ExifValue = ExifNumber !Int
 	-- sometimes as float (exposure compensation, we rather think -0.75)
 	-- 'show' will display it as 1/160.
 	| ExifNumberList ![Int]
+	-- ^ List of numbers. Originally they could have been short, int, signed or not.
 	| ExifRationalList ![(Int,Int)]
-	| ExifUndefined BS.ByteString
-	| ExifUnknown !Word16 !Int !Int -- type, count then value
+	-- ^ A list of rational numbers (numerator, denominator).
+	-- Sometimes we're used to it as rational (exposition time: 1/160),
+	-- sometimes as float (exposure compensation, we rather think -0.75)
+	-- 'show' will display it as 1/160.
+	| ExifUndefined !BS.ByteString
+	-- ^ The undefined type in EXIF means that the contents are not
+	-- specified and up to the manufacturer. In effect it's exactly
+	-- a bytestring. Sometimes it's text with ASCII or UNICODE at the
+	-- beginning, often it's binary in nature.
+	| ExifUnknown !Word16 !Int !Int
 	-- ^ Unknown exif value type. All EXIF 2.3 types are
 	-- supported, it could be a newer file.
+	-- The parameters are type, count then value
 	deriving Eq
 
 instance Show ExifValue where
